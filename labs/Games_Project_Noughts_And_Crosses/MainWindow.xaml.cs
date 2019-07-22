8943695f78d579bf3335eb416c4daa6dd43c60d3
Sam <sige@spartaglobal.com>
2019-07-22 20:42:38 +0100

    Add Button Iteration

--- labs/Games_Project_Noughts_And_Crosses/MainWindow.xaml.cs
+++ labs/Games_Project_Noughts_And_Crosses/MainWindow.xaml.cs
@@ -21,7 +21,20 @@ namespace Games_Project_Noughts_And_Crosses
     public partial class MainWindow : Window
     {
         #region Private Members
+        /// <summary>
+        /// Holds the current results of cells in the active game
+        /// </summary>
+        private MarkType[] nResults;
 
+        /// <summary>
+        /// True if it is Player 1's Turn (X) or Player 2's Turn (O)
+        /// </summary>
+        private bool Player1Turn;
+
+        /// <summary>
+        /// True if the game has ended
+        /// </summary>
+        private bool GameHasEnded;
         #endregion
 
         #region Constructor
@@ -37,7 +50,20 @@ namespace Games_Project_Noughts_And_Crosses
 
         private void NewGame()
         {
+            //Create a new blank array of free cells
+            nResults = new MarkType[9];
 
+            for (var i = 0; i < nResults.Length; i++)
+                nResults[i] = MarkType.Free;
+
+            //Make sure that Player 1 starts the game
+            Player1Turn = true;
+
+            //Iterate every button on Grid.
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+
+            });
         }
     }
 }
