86badbf5b3c5866a51acba93dd2a9edb433b38ce
Sam <sige@spartaglobal.com>
2019-07-23 11:37:19 +0100

    Database created for Leaderboard

---
+++ labs/Game_Project_Database.sql
@@ -0,0 +1,16 @@
+﻿drop database if exists GameLeaderboard
+drop table if exists MainLeaderboard
+CREATE DATABASE GameLeaderboard
+
+go
+
+use GameLeaderboard
+CREATE TABLE MainLeaderboard (playername varchar(50) NOT NULL ,
+							 Wins int,
+							 Losses int)
+go
+
+DELETE FROM MainLeaderboard Where playername = 'Sam'
+INSERT INTO MainLeaderboard (playername) Values ('Sam')
+
+select*from MainLeaderboard
