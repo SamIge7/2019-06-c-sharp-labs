2a649a9b9e9666a0138610e1a2fa4e39b6e213ea
Sam <sige@spartaglobal.com>
2019-07-23 14:30:26 +0100

    Leaderboard Page Created

---
+++ labs/Games_Project_Noughts_And_Crosses/Leaderboard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Games_Project_Noughts_And_Crosses
+{
+    /// <summary>
+    /// Interaction logic for Leaderboard.xaml
+    /// </summary>
+    public partial class Leaderboard : Window
+    {
+        List<MainLeaderboard> mainLeaderboards;
+        public Leaderboard()
+        {
+            InitializeComponent();
+            Initialise();
+        }
+
+        void Initialise()
+        {
+            using (var db = new GameLeaderboardEntities())
+            {
+                mainLeaderboards = db.MainLeaderboard.ToList();
+            }
+            Leaderboard.ItemsSource = mainLeaderboards;
+        }
+    }
+}
