7ce592fb6590c6bf876d3abf7a6bca91a57490bc
Sam <sige@spartaglobal.com>
2019-07-24 13:52:14 +0100

    Added feature to create new player in the database

--- labs/Games_Project_Noughts_And_Crosses/TitleWindow.xaml.cs
+++ labs/Games_Project_Noughts_And_Crosses/TitleWindow.xaml.cs
@@ -38,7 +38,37 @@ namespace Games_Project_Noughts_And_Crosses
 
         private void BeginGame_Click(object sender, RoutedEventArgs e)
         {
-            
+            if(Player1Name.Text == "" || Player2Name.Text == "")
+            {
+                MessageBox.Show("Make sure both players have put their names in");
+            }
+            else
+            {
+                string player1name = Player1Name.Text;
+                string player2name = Player2Name.Text;
+
+                int p1wins = 0;
+                int p1losses = 0;
+                int p2wins = 0;
+                int p2losses = 0;
+
+                using (var db = new GameLeaderboardEntities())
+                {
+                    MainLeaderboard newplayer1 = new MainLeaderboard();
+                    newplayer1.playername = Player1Name.Text;
+                    newplayer1.Wins = p1wins;
+                    newplayer1.Losses = p1losses;
+                    db.MainLeaderboard.Add(newplayer1);
+
+                    MainLeaderboard newplayer2 = new MainLeaderboard();
+                    newplayer2.playername = Player2Name.Text;
+                    newplayer2.Wins = p2wins;
+                    newplayer2.Losses = p2losses;
+                    db.MainLeaderboard.Add(newplayer2);
+                    db.SaveChanges();
+                }
+            }
+
             Game game = new Game();
             this.Visibility = Visibility.Hidden;
             game.Show();
