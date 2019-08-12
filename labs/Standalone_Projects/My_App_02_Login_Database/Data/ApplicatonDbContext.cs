bb2befbcc9f0cd3a95b6ba303f4794b2d4edda69
Sam <sige@spartaglobal.com>
2019-08-12 13:53:43 +0100

    added Models and Data folder

---
+++ labs/Standalone_Projects/My_App_02_Login_Database/Data/ApplicatonDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using My_App_02_Login_Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_App_02_Login_Database.Data
+{
+    public class ApplicatonDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicatonDbContext(DbContextOptions<ApplicatonDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ labs/Standalone_Projects/My_App_02_Login_With_Database_Following_Tutorial/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Sitecore.FakeDb;
+using EO.Internal;
+
+namespace My_App_02_Login_With_Database_Following_Tutorial.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Validate(Admins admin)
+        {
+            var _admin = db.Admins.Where(s => s.E)
+        }
+    }
+}
