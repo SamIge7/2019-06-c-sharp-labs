c574c8f345165c33eceeb08d7479d4b5b905ed54
Sam <sige@spartaglobal.com>
2019-08-12 16:23:25 +0100

    Added database

---
+++ labs/Standalone_Projects/My_App_02_Database/Data/My_App_02_DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace My_App_02_Database.Models
+{
+    public class My_App_02_DatabaseContext : DbContext
+    {
+        public My_App_02_DatabaseContext (DbContextOptions<My_App_02_DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<My_App_02_Database.Models.Users> Users { get; set; }
+    }
+}
---
+++ labs/Standalone_Projects/My_App_02_Database/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_App_02_Database.Models
+{
+    public class Users
+    {
+        public int UsersID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
