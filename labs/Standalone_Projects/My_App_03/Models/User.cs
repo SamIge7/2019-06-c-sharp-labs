778106aa23d24d6ab3785bf9c706c3d5a1e6ba25
Sam <sige@spartaglobal.com>
2019-08-13 10:15:40 +0100

    Updated User Model

--- labs/Standalone_Projects/My_App_03/Models/User.cs
+++ labs/Standalone_Projects/My_App_03/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,30 @@ namespace My_App_03.Models
 {
     public class User
     {
+        [Key]
         public int UserID { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
         public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [Display(Name = "Email ID")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
+
+        [Display(Name = "Mobile")]
         public string Mobile { get; set; }
     }
 }
