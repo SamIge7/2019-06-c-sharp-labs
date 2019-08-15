1c28fff3a63ef62842ad9877e4436268a4b076ad
Sam <sige@spartaglobal.com>
2019-08-15 09:48:14 +0100

    Created User Model

---
+++ labs/Standalone_Projects/My_App-03d/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace My_App_03d.Models
+{
+    public class User
+    {
+        [Key]
+        [Display(Name = "User ID")]
+        public int UserID { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
