daadb932ea0247d9eeee53263bfef4a17d6e6950
Sam <sige@spartaglobal.com>
2019-08-19 12:29:54 +0100

    Pluralsight Tutorial

---
+++ labs/McMovie_Rick_Anderson_Tutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McMovie_Rick_Anderson_Tutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ labs/McMovie_Rick_Anderson_Tutorial/Data/McMovie_Rick_Anderson_TutorialContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace McMovie_Rick_Anderson_Tutorial.Models
+{
+    public class McMovie_Rick_Anderson_TutorialContext : DbContext
+    {
+        public McMovie_Rick_Anderson_TutorialContext (DbContextOptions<McMovie_Rick_Anderson_TutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<McMovie_Rick_Anderson_Tutorial.Models.Movie> Movie { get; set; }
+    }
+}
