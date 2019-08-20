b555657e6c27e1bea927105ae10bc6031ac6d345
Sam <sige@spartaglobal.com>
2019-08-20 09:56:51 +0100

    Created Method Validations

--- labs/BethanysPieShop/Controllers/FeedbackController.cs
+++ labs/BethanysPieShop/Controllers/FeedbackController.cs
@@ -22,5 +22,21 @@ namespace BethanysPieShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            if(ModelState.IsValid)
+            {
+                _feedbackRepository.AddFeedback(feedback);
+                return RedirectToAction("FeedbackComplete");
+            }
+            return View(feedback);
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
     }
 }
