76624280214e65fbb744dbb80916eadf30c2c956
Sam <sige@spartaglobal.com>
2019-08-21 10:30:54 +0100

    Created the Home Controller

--- labs/Standalone_Projects/BethanysPieShop/Controllers/HomeController.cs
+++ labs/Standalone_Projects/BethanysPieShop/Controllers/HomeController.cs
@@ -10,6 +10,13 @@ namespace BethanysPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
