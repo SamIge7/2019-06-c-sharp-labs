33d2b5b7766602269fd1e6098e9e800db1a94a0a
Sam <sige@spartaglobal.com>
2019-08-21 13:59:48 +0100

    Use the Library Manager to use Bootstrap

--- labs/Standalone_Projects/BethanysPieShop/Controllers/HomeController.cs
+++ labs/Standalone_Projects/BethanysPieShop/Controllers/HomeController.cs
@@ -19,7 +19,13 @@ namespace BethanysPieShop.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ labs/Standalone_Projects/BethanysPieShop/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
